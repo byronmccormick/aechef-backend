27c2d347dc9f336c21c5c8f3b3654edacc336eff
byron mccormick <byronjamesmccormick@gmail.com>
2024-08-08 10:59:31 +1200

    adding checkout flow

--- src/controllers/RestaurantController.ts
+++ src/controllers/RestaurantController.ts
@@ -1,6 +1,23 @@
 import {Request, Response} from "express";
 import Restaurant from "../models/restaurant";
 
+const getRestaurant = async (req: Request, res: Response) => {
+    try {
+        const restaurantId = req.params.restaurantId;
+
+        const restaurant = await Restaurant.findById(restaurantId);
+
+        if(!restaurant){
+            return res.status(404).json({messahe: "Restaurant could not be found"});
+        }
+
+        res.json(restaurant);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "Something went wrong"});
+    }
+}
+
 const searchRestaurant = async (req: Request, res: Response) => {
     try{
         const city = req.params.city;
@@ -63,4 +80,5 @@ const searchRestaurant = async (req: Request, res: Response) => {
 
 export default {
     searchRestaurant,
+    getRestaurant,
 }
--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import {v2 as cloudinary} from "cloudinary";
-import restaurantRoute from "./routes/RestaurantRoute"
-
+import restaurantRoute from "./routes/RestaurantRoute";
+import orderRoute from "./routes/OrderRoute";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=> console.log("Connected to database!"));
 
@@ -17,9 +17,11 @@ cloudinary.config({
 });
 
 const app = express();
-app.use(express.json());
 app.use(cors());
 
+app.use("/api/order/checkout/webhook", express.raw({type: "*/*"}));
+
+app.use(express.json());
 
 app.get("/health", async (req:Request, res: Response)=>{
     res.send({message: "health OK!"});
@@ -28,7 +30,8 @@ app.get("/health", async (req:Request, res: Response)=>{
 
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
-app.use("/api/restaurant", restaurantRoute)
+app.use("/api/restaurant", restaurantRoute);
+app.use("/api/order", orderRoute);
 
 app.listen(7000, ()=> {
     console.log("Server is running. Better go catch it!")
