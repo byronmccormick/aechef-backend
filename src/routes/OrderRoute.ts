08126b330019c4ec6706712adc476af65f02eeed
byron mccormick <byronjamesmccormick@gmail.com>
2024-08-08 15:05:41 +1200

    adding view order features

--- src/routes/OrderRoute.ts
+++ src/routes/OrderRoute.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.post("/checkout/create-checkout-session", jwtCheck, jwtParse, OrderController.createCheckoutSession);
 router.post("/checkout/webhook", OrderController.stripeWebhookHandler);
+router.get("/", jwtCheck, jwtParse, OrderController.getMyOrders);
 
 export default router;
